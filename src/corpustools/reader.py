30b737dce68bc60a432c810d011a6c260e93dd77
Lázaro Raúl <riglesiasvera@gmail.com>
2021-05-12 19:01:01 -0400

    Remove the `file_corpus` flag
    This flag is not necessary. Is a corpus is a folder with multiple files, then the driver should read every one of them

--- src/corpustools/reader.py
+++ src/corpustools/reader.py
@@ -1,19 +1,15 @@
 from .drivers import get_driver
 
 class CorpusReader:
-    def __init__(self, addr, driver, file_corpus):
+    def __init__(self, addr, driver):
         self.data = []
         self.addr = addr
-        self.file_corpus = file_corpus
         self.driver = get_driver(driver)
         
     def get_data(self):
-        if self.file_corpus: self.read(self.addr)
-        else: self.read_addr(self.addr)
+        self.read(self.addr)
         return self.data
 
     def read(self, addr):
         self.data.extend(self.driver(addr))
-
-    def read_addr(self, addr):
-        raise NotImplementedError()
+        return self
