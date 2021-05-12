2037a081b1814568e78693244c7becf9a717a654
Lázaro Raúl <riglesiasvera@gmail.com>
2021-05-12 16:36:01 -0400

    Read a single corpus file

--- src/corpustools/reader.py
+++ src/corpustools/reader.py
@@ -2,14 +2,18 @@ from .drivers import get_driver
 
 class CorpusReader:
     def __init__(self, addr, driver, corpus_type):
+        self.data = []
         self.addr = addr
+        self.corpus_type = corpus_type
         self.driver = get_driver(driver)
-        self.corpus = corpus_type
-        if corpus_type == 'file': self.read(addr)
+        
+    def get_data(self):
+        if self.corpus_type == 'file': self.read(addr)
         else: self.read_addr(addr)
+        return self.data
 
-    def read(self):
-        raise NotImplementedError()
+    def read(self, addr):
+        self.data.extend(self.driver.read(addr))
 
     def read_addr(self, addr):
         raise NotImplementedError()
