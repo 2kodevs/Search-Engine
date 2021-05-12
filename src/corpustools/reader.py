4269e670cc715f21b7d961c28d82f5789a175178
Lázaro Raúl <riglesiasvera@gmail.com>
2021-05-12 18:30:27 -0400

    CorpusReader `read` method tested
    Changes:
    - Use a `file_corpus` bool instead of a `corpus_type` str
    - Fix erros using `self.addr`

--- src/corpustools/reader.py
+++ src/corpustools/reader.py
@@ -1,19 +1,19 @@
 from .drivers import get_driver
 
 class CorpusReader:
-    def __init__(self, addr, driver, corpus_type):
+    def __init__(self, addr, driver, file_corpus):
         self.data = []
         self.addr = addr
-        self.corpus_type = corpus_type
+        self.file_corpus = file_corpus
         self.driver = get_driver(driver)
         
     def get_data(self):
-        if self.corpus_type == 'file': self.read(addr)
-        else: self.read_addr(addr)
+        if self.file_corpus: self.read(self.addr)
+        else: self.read_addr(self.addr)
         return self.data
 
     def read(self, addr):
-        self.data.extend(self.driver.read(addr))
+        self.data.extend(self.driver(addr))
 
     def read_addr(self, addr):
         raise NotImplementedError()
