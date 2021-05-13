f0cd9289238ca11939ce551fc71b5e7e30c2ae29
Miguel Tenorio <mtpotro41@gmail.com>
2021-05-12 22:34:30 -0400

    [indexer] refs #2 - Update tokenizer and update_vocabulary methods.
    Add some logs too

--- src/indexer/Indexer.py
+++ src/indexer/Indexer.py
@@ -1,35 +1,48 @@
 import json
+from ..logging import LoggerFactory as Logger
+
+log = None
+stopwords = [',', '.', ';', ':', '\"', '\'', '(', ')', '-', '\n'] #//TODO: Determine best place for this
 
 class Indexer:
 
     def __init__(self, idx_dir = './indices/'):
+        global log
+        log = Logger('Search-Engine').getChild('Indexer')
+
         self.N = 0
         self.max_freq = []
         self.vocabulary = dict()
         self.idx_dir = idx_dir
 
-    def index(self):
+
+    def index(self, corpus_dir, driver):
         data = [] #call to get_data() of corpustools 
 
         self.N = len(data)
         self.max_freq = [0] * self.N
 
-        terms = Indexer.get_raw_terms(data)
+        terms = Indexer.tokenize(data)
         self.update_vocabulary(terms)
-        self.save_index()
+        self.save_index(driver)
 
 
-    def save_index(self):
+    def save_index(self, corpus_name):
         import os
         if not os.path.exists(self.idx_dir):
             os.mkdir(self.idx_dir)
-            
+            log.info(f'Created folder {self.idx_dir}')
+
         files_count = len([name for name in os.listdir(self.idx_dir) if os.path.isfile(name)])
-        with os.open(self.idx_dir + f'index_{files_count}') as fd:
+        with os.open(self.idx_dir + f'{corpus_name}_index_{files_count}') as fd:
             fd.write(self.to_json())
+            log.info('Created index ' + f'{corpus_name}_index_{files_count}' + f' at {self.idx_dir}')
+
 
     def update_vocabulary(self, terms):
-        unique_terms = { word for (word, _) in terms }
+        terms.append(('zzzzz$$$$$&&&&&&', -1))
+        unique_terms = list({ word for (word, _) in terms })
+        unique_terms.sort()
         last_idx = 0
 
         for term in unique_terms:
@@ -37,18 +50,23 @@ class Indexer:
             freq = 0
             for i in range(last_idx, len(terms)):
                 word, id = terms[i]
-                if (word != term):
-                    break
-                if (id != idx):
-                    if (not self.vocabulary[word]):
-                        self.vocabulary[word] = []
-                    self.vocabulary[word].append((idx, freq))
-                    self.max_freq[idx] = max(freq, self.max_freq[idx])
+
+                if id != idx or word != term:
+                    if (not self.vocabulary.get(term)):
+                        self.vocabulary[term] = []
+                    self.vocabulary[term].append((idx, freq))
+                    self.max_freq[idx - 1] = max(freq, self.max_freq[idx - 1])
+                    if word != term:
+                        last_idx = i
+                        break
                     idx = id
                     freq = 0
                 
                 freq += 1
                 last_idx = i
+        log.debug('Vocabulary updated')
+        log.debug(f'Vocabulary: {self.vocabulary}')
+
 
     def to_json(self):
         temp = {
@@ -60,7 +78,7 @@ class Indexer:
 
 
     @staticmethod
-    def get_raw_terms(data):
+    def tokenize(data):
         terms = []
         for doc in data:
             id = doc['ID']
@@ -68,10 +86,18 @@ class Indexer:
 
             #//TODO: Consider to give more weight to terms in Title
             for word in body.split(' '):
-                if word is None: #for text with several spaces
+
+                for sw in stopwords:
+                    if sw in word:
+                        word = word.replace(sw, '')
+
+                if word == '': #for text with several spaces and blanck lines
                     continue
+
                 terms.append((word, id))
             if doc['Author'] != '':
                 terms.append((doc['Author'], id))
         terms.sort()
+        log.debug('Corpus tokenized')
+        log.debug(f'Terms: {terms}')
         return terms
