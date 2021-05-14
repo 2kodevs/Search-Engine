de2ca436377c2a95cefcb85487666d42d955eef9
Lázaro Raúl <riglesiasvera@gmail.com>
2021-05-14 14:24:12 -0400

    [reader] refs #12 - Save the documents printeable data

--- src/corpustools/reader.py
+++ src/corpustools/reader.py
@@ -1,14 +1,40 @@
 from .drivers import get_driver
+import json, os
 
 class CorpusReader:
-    def __init__(self, addr, driver):
+    def __init__(self, addr, driver, parse_dir='./.parse/'):
         self.data = []
         self.addr = addr
+        self.parse_dir = parse_dir
         self.driver = get_driver(driver)
+
+        if not os.path.exists(self.parse_dir):
+            os.mkdir(self.parse_dir)
+
+    def get_dir(self):
+        addr = self.addr.replace('/','-')
+        return f'{self.parse_dir}{self.driver.__name__.lower()}-{addr}'
         
     def get_data(self):
         self.read(self.addr)
+
+        tosave = ['id', 'title', 'author']
+        self.simple = list(map(lambda item: {x:item[x] for x in tosave}, self.data))
+        with open(self.get_dir(), 'w') as fd:
+            json.dump(self.simple, fd)
         return self.data
+
+    def get_info(self, rank):
+        try:
+            with open(self.get_dir(), 'r') as fd:
+                self.simple = json.load(fd)
+        except FileNotFoundError:
+            self.get_data()
+        details = []
+        for (_, id) in rank:
+            info = self.simple[id - 1]
+            details.append((info['title'], info['author']))
+        return details
 
     def read(self, addr):
         self.data.extend(self.driver(addr))
