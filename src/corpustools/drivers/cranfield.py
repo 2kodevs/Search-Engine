f664b472a69bcff827e356e2efedf789d28c0a42
Lázaro Raúl <riglesiasvera@gmail.com>
2021-05-14 15:29:46 -0400

    [driver] refs #14 - Add support for parse cran test queries

--- src/corpustools/drivers/cranfield.py
+++ src/corpustools/drivers/cranfield.py
@@ -2,6 +2,8 @@ import re
 
 txtblock = "\n((?:(?=[^.])(?:.*\n))*)" 
 docre = rf'\.I (.+)\n\.T{txtblock}\.A{txtblock}\.B{txtblock}\.W{txtblock}'
+qryre = rf'\.I .+\n\.W{txtblock}'
+relre = r'(.+) (.+) .+\n'
 
 class Cranfield:
     '''
@@ -19,16 +21,37 @@ class Cranfield:
     '''
     @staticmethod
     def read(addr):
-    with open(addr, 'r') as fd:
-        doctext = fd.read()
-    matchings = re.findall(docre, doctext)
-    return [
-        {
-            'id':     id,
-            'B':      b.lower(),
-            'text':   text.lower(),
-            'title':  title.lower(),
-            'author': author.lower(),
-        } 
-        for (id, title, author, b, text) in matchings
-    ]
+        with open(addr, 'r') as fd:
+            doctext = fd.read()
+        matchings = re.findall(docre, doctext)
+        return [
+            {
+                'id':     id,
+                'B':      b.lower(),
+                'text':   text.lower(),
+                'title':  title.lower(),
+                'author': author.lower(),
+            } 
+            for (id, title, author, b, text) in matchings
+        ]
+
+    
+    @staticmethod
+    def queries(query, rel):
+        with open(query, 'r') as fd:
+            querytext = fd.read()
+        with open(query, 'r') as fd:
+            reltext = fd.read()
+        q = re.findall(qryre, querytext)
+        r = re.findall(relre, reltext)
+        data = [
+            {
+                'rel':   [],
+                'id':    str(id + 1),
+                'query': text.lower(),
+            } 
+            for (id, text) in enumerate(q)
+        ]
+        for id, a in r:
+            data[int(id) - 1]['rel'].append(a)
+        return data
