65e5665cbd5abc4c4650dfd08673742242d216cd
Lázaro Raúl <riglesiasvera@gmail.com>
2021-05-14 00:22:08 -0400

    Initial main structure and args

--- main.py
+++ main.py
@@ -1,17 +1,41 @@
 from src import Indexer, LoggerFactory as Logger
 
-if __name__ == '__main__':
-    #//TODO: Get args
-    args = None
 
+def init_logger(args):
     log = Logger(name='Search-Engine', log=args.file)
     log.setLevel(args.level)
+    return log
 
-    corpus_addr = args.dir
-    driver = args.driver
 
-    idx = Indexer()
-    
-    data = idx.get_index(corpus_addr, driver)
-    print(data)
-    
+def visual(args):
+    log = init_logger(args)
+    log.info('Starting the app', 'visual')
+
+
+def cmd(args):
+    log = init_logger(args)
+    log.info('Running the indexer', 'cmd')
+
+
+if __name__ == '__main__':
+    import argparse
+
+    parser = argparse.ArgumentParser(description="Search Engine parser")
+    subparsers = parser.add_subparsers()
+
+    cmdline = subparsers.add_parser('cmd', help="Solve a query from cmd")
+    cmdline.add_argument('-d', '--driver', type=str,  required=True,       help='driver to use in the corpus parsing proccess')
+    cmdline.add_argument('-c', '--corpus', type=str,  required=True,       help='corpus address')
+    cmdline.add_argument('-f', '--file',   type=bool, action='store_true', help='use the logs file')
+    cmdline.add_argument('-l', '--level',  type=str,  default='INFO',      help='log level')
+    cmdline.add_argument('-q', '--query',  type=str,  required=True,       help='query to retrive')
+    cmdline.set_defaults(command=cmd)
+
+    app = subparsers.add_parser('visual', help="Open the visual application")
+    app.set_defaults(file=True)
+    app.set_defaults(level='INFO')
+    app.set_defaults(command=visual)
+
+    args = parser.parse_args()
+    if not hasattr(args, 'command'): parser.print_help()
+    else: args.command(args)
